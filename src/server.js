3cc69a473e6d43c8b2155b3dea8101ea7f47e5d3
An Nguyen <anndb1903@gmail.com>
2025-03-13 20:54:35 +0700

    video 21

--- src/server.js
+++ src/server.js
@@ -5,19 +5,21 @@ require('dotenv').config()
 
 console.log(">>> check env: ", process.env)
 
-const app = express()
+const app = express() // app express
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 
+
+//config template engine
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', './ejs')
- 
+
+//config static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Khai báo router
 app.get('/', (req, res) => {
   res.send('Hello World! and bao an dep trai')
-})
-
-app.get('/abc', (req, res) => {
-    res.send('Check abc')
 })
 
 app.get('/baoan', (req, res) => {
