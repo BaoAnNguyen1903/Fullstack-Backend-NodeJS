cd29665c036413675812a4221c8935752fdd9b6d
An Nguyen <anndb1903@gmail.com>
2025-03-13 16:52:21 +0700

    video 18

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', './ejs')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -8,7 +12,11 @@ app.get('/', (req, res) => {
 
 app.get('/abc', (req, res) => {
     res.send('Check abc')
-  })
+})
+
+app.get('/baoan', (req, res) => {
+  res.render('sample.ejs')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
