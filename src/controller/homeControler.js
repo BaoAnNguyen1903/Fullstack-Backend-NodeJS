5e09a6a877f841646ed78cdf2b12214b2bf85b7f
An Nguyen <anndb1903@gmail.com>
2025-03-15 14:32:43 +0700

    #21 Connection cost - Tạo mới connection

--- src/controller/homeControler.js
+++ src/controller/homeControler.js
@@ -1,8 +1,21 @@
+const connection = require("../config/database");
+const { use } = require("../routes/web");
+
 const getHomepage = (req, res) => {
-    res.send('Hello World! and bao an dep trai')
+    let users = [];
+
+    connection.query(
+        'SELECT * FROM Users',
+        function (err, results, fields) {
+            users = results;
+            console.log(">>> results= ", results);
+            console.log(">>> check users: ", users)
+            res.send(JSON.stringify(users)) // hàm này để coverage ra chuỗi string vì users là object
+        }
+    );
 }
 
-const getBaoAn = () => {
+const getBaoAn = (req, res) => {
     res.send('Hello World! and bao an dep trai vcl hahahahahhahaha')
 }
 
