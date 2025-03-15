5b015a1eebf8d692a00c1d3f26854ae4147ea89f
An Nguyen <anndb1903@gmail.com>
2025-03-15 14:20:02 +0700

    #31. Tái sử dụng connection

--- src/server.js
+++ src/server.js
@@ -6,8 +6,7 @@ const webRoutes = require('./routes/web')
 const app = express() // app express
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
-//get the client
-const mysql = require('mysql2');
+const connection = require('./config/database')
 
 //config
 configViewEngine(app);
@@ -15,21 +14,11 @@ configViewEngine(app);
 //route
 app.use('/', webRoutes);
 
-//test connection
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3007, //default: 3006 nhưng đang chạy db bằng docker nên chạy 3007
-  user: 'root',
-  password: "123456",
-  database: 'hoidanit'
-});
-
 //querry
 connection.query(
   'SELECT * FROM Users',
   function (err, results, fields) {
     console.log(">>> results= ", results);
-    console.log(">>> fields= ", fields);
   }
 );
 
