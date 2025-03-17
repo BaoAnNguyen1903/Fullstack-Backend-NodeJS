dbe0034bde98961dae39cd1bacaf5b84acc01256
An Nguyen <anndb1903@gmail.com>
2025-03-17 10:44:41 +0700

    first commit

--- src/config/database.js
+++ src/config/database.js
@@ -3,15 +3,6 @@ require('dotenv').config(); // import khi xai` process.env
 //get the client
 const mysql = require('mysql2/promise'); // thêm promise để k phải viết dưới dạng callback
 
-//test connection
-// const connection = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT, //default: 3006 nhưng đang chạy db bằng docker nên chạy 3007
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-// });
-
 const connection = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT, //default: 3006 nhưng đang chạy db bằng docker nên chạy 3007
