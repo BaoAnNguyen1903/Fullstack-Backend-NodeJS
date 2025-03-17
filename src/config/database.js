dd27635bf7f7970fa4c7066f21dfcb19ea8d9c77
An Nguyen <anndb1903@gmail.com>
2025-03-17 15:49:25 +0700

    #59. Create database

--- src/config/database.js
+++ src/config/database.js
@@ -5,31 +5,31 @@ const mongoose = require('mongoose'); // thêm promise để k phải viết dư
 
 const dbState = [{
     value: 0,
-    label: "disconnected"
+    label: "Disconnected"
 },
 {
     value: 1,
-    label: "connected"
+    label: "Connected"
 },
 {
     value: 2,
-    label: "connecting"
+    label: "Connecting"
 },
 {
     value: 3,
-    label: "disconnecting"
+    label: "Disconnecting"
 }];
 
 const connection = async () => {
-        const options = {
-            user: process.env.DB_USER, // muốn kết nối db để tăng hiệu năng thì dùng dbpool.
-            //  thư viện viết bằng ts nên truy cập thẳng vào thư viện để xem những tham số cần truyền vào
-            pass: process.env.DB_PASSWORD,
-
-        }
-        await mongoose.connect(process.env.DB_HOST, options);
-        const state = Number(mongoose.connection.readyState);
-        console.log(dbState.find(f => f.value == state).label, "to db"); // connected to db
+    const options = {
+        user: process.env.DB_USER, // muốn kết nối db để tăng hiệu năng thì dùng dbpool.
+        //  thư viện viết bằng ts nên truy cập thẳng vào thư viện để xem những tham số cần truyền vào
+        pass: process.env.DB_PASSWORD,
+        dbName: process.env.DB_NAME
+    }
+    await mongoose.connect(process.env.DB_HOST, options);
+    const state = Number(mongoose.connection.readyState);
+    console.log(dbState.find(f => f.value === state).label, "to database"); // connected to db
 }
   
 module.exports = connection;
--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 const connection = require('./config/database')
 const app = express() // app express
+const mongoose = require('mongoose');
 
 // config req.body
 app.use(express.json()) // for json
@@ -17,6 +18,13 @@ configViewEngine(app);
 //route
 app.use('/', webRoutes);
 
+const kittySchema = new mongoose.Schema({
+  name: String
+});
+
+const Kitten = mongoose.model('Kitten', kittySchema);
+const cat = new Kitten({ name: 'bao an cat' });
+cat.save();
 //anonimot function (() => {})viết sai chính tả rr nên viết cho đọc thôi
 (async() => { //seftruning function 
   //test connection
