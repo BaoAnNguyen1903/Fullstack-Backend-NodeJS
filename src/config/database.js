4780d89d42992150f3c3491d56d9c87b1a43387d
An Nguyen <anndb1903@gmail.com>
2025-03-17 13:32:54 +0700

    #57. Create connection db

--- src/config/database.js
+++ src/config/database.js
@@ -1,23 +1,31 @@
 require('dotenv').config(); // import khi xai` process.env
 
 //get the client
-const mysql = require('mysql2/promise'); // thêm promise để k phải viết dưới dạng callback
+const mongoose = require('mongoose'); // thêm promise để k phải viết dưới dạng callback
 
-// const connection = mysql.createPool({
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT, //default: 3006 nhưng đang chạy db bằng docker nên chạy 3007
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0
-// });
+const dbState = [{
+    value: 0,
+    label: "disconnected"
+},
+{
+    value: 1,
+    label: "connected"
+},
+{
+    value: 2,
+    label: "connecting"
+},
+{
+    value: 3,
+    label: "disconnecting"
+}];
 
 const connection = async () => {
     // Or:
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/test');
+        await mongoose.connect('mongodb://root:123456@localhost:27018');
+        const state = Number(mongoose.connection.readyState);
+        console.log(dbState.find(f => f.value == state).label, "to db"); // connected to db
     } catch (error) {
         handleError(error); // lưu lỗi xuống db
     }
