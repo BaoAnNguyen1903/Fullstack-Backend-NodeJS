9f4acd7716b6645dd09b994229736c7e54655cc4
An Nguyen <anndb1903@gmail.com>
2025-03-17 16:40:23 +0700

    #60. Create schema & model

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 const connection = require('./config/database')
 const app = express() // app express
-const mongoose = require('mongoose');
+const Kitten = require('./model/Kitten');
 
 // config req.body
 app.use(express.json()) // for json
@@ -18,13 +18,10 @@ configViewEngine(app);
 //route
 app.use('/', webRoutes);
 
-const kittySchema = new mongoose.Schema({
-  name: String
-});
-
-const Kitten = mongoose.model('Kitten', kittySchema);
-const cat = new Kitten({ name: 'bao an cat' });
+//gioong nhu 1 repo
+const cat = new Kitten({ name: 'bao an cat model' });
 cat.save();
+
 //anonimot function (() => {})viết sai chính tả rr nên viết cho đọc thôi
 (async() => { //seftruning function 
   //test connection
