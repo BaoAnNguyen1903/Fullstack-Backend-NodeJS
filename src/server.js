3dbfd5f9058d7476227ecee0553bf3de21c2e160
An Nguyen <anndb1903@gmail.com>
2025-03-24 17:01:21 +0700

    #84. Setup fileupload by express-fileupload

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
-require('dotenv').config()
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
 const configViewEngine = require('./config/viewEngine');
-const webRoutes = require('./routes/web')
-const apiRoutes = require('./routes/api')
+const webRoutes = require('./routes/web');
+const apiRoutes = require('./routes/api');
+const fileUpload = require('express-fileupload');
+const connection = require('./config/database');
+const app = express(); // app express
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
-const connection = require('./config/database')
-const app = express() // app express
+
+//config fileupload
+//luu y: phai config phia tren route
+//default options
+app.use(fileUpload());
 
 // config req.body
 app.use(express.json()) // for json
