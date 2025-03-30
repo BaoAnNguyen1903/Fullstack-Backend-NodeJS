48ce0342b4c7e95153933db79981e4b22fb77b8e
An Nguyen <anndb1903@gmail.com>
2025-03-30 15:17:31 +0700

    coment src code

--- src/service/taskService.js
+++ src/service/taskService.js
@@ -13,9 +13,11 @@ module.exports = {
     getTasks: async (queryString) => {
         const page = queryString.page;
         const {filter, limit} = aqp(queryString);
-        delete filter.page;
+        delete filter.page; // page bị thừa vì FE sẽ gửi client sẽ truyền page lên để tính ra skip. nếu truyền page và thì sẽ hiểu là 1 thuộc tính và muốn query nên xóa
         let offset = (page - 1) * limit;
-        result = await Task.find(filter).skip(offset).limit(limit).exec();
+        result = await Task.find(filter).skip(offset).limit(limit).exec(); //skip() là 1 hàm của mongoose .exec là 1 hàm thực thi đảm bảo đúng là 1 promise
+        //queryString sẽ trả ra gtri chính xác nếu chúng t truyền như này name=baoan thì chính xác 100% là baoan k có thừa thiếu
+        // nếu chúng t truyền name=/baoan/ thì tất cả thằng nào có chữ baoan sẽ được query
         return result;
     },
 
@@ -25,6 +27,7 @@ module.exports = {
     },
 
     deleteTask: async (taskId) => {
+        // let result = await Customer.deleteOne({_id: customerId}); đây là xóa luôn trong db
         let result = await Task.deleteById(taskId); // hàm delete của thư viện mongoose-delete set true false
         return result;
     }
