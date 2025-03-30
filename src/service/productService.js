551a5cfd4305185f8e7c4d2e4cdf608b602db01d
An Nguyen <anndb1903@gmail.com>
2025-03-30 18:05:55 +0700

    #120. ADD a task to project

--- src/service/productService.js
+++ src/service/productService.js
@@ -18,6 +18,17 @@ module.exports = {
             return newResult;
         }
 
+        if (data.type === "ADD-TASKS") {
+            let myProject = await Project.findById(data.projectId).exec();// projectId tên đặt trong postman
+            for (let i = 0; i < data.taskArr.length; i++) {
+                myProject.tasks.push(data.taskArr[i]);
+            }
+
+            let newResult = await myProject.save();
+            return newResult;
+        }
+
+
         if (data.type === "REMOVE-USERS") {
             let myProject = await Project.findById(data.projectId).exec();
 
